473ad99eff80f7fe537365a2666ccdb95ea612bd
DmitryRad <server@internet.ru>
2022-09-03 20:10:00 +0300

    small edits

--- src/index.js
+++ src/index.js
@@ -1,40 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, bindActionCreators } from 'redux';
-import reducer from './reducer';
-import * as actions from './actions';
 
-const store = createStore(reducer);
-
-const { dispatch, subscribe, getState } = store;
-
-const update = () => {
-    document.getElementById('counter').textContent = getState().value;
-}
-
-subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//     dispatch(creator(...args));
-// }
-
-const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-
-document.getElementById('inc').addEventListener('click', inc);
-
-document.getElementById('dec').addEventListener('click', dec);
-
-document.getElementById('rnd').addEventListener('click', () => {
-    const value = Math.floor(Math.random() * 10);
-    rnd(value);
-});
+console.log('Hello Redux!')
 
 ReactDOM.render(
   <React.StrictMode>
-   <>
-   </>
+    <>
+    
+    </>
   </React.StrictMode>,
-    document.getElementById('root')
+  document.getElementById('root')
 );

93d924d733247d42713f63945887c97f8b4cb64e
wDmitry <server@internet.ru>
2022-09-05 11:15:21 +0300

    connect React and Redux use connect

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,36 @@
-const Counter = ({ counter, inc, dec, rnd }) => {
-    return (
-        <div className="jumbotron">
+import { Component } from 'react';
+import { connect }  from "react-redux";
+import * as actions from '../actions';
+
+// const Counter = ({ counter, inc, dec, rnd }) => {
+//     return (
+//         <div className="jumbotron">
+//             <h1>{counter}</h1>
+//             <button onClick={dec} className="btn btn-primacy">DEC</button>
+//             <button onClick={inc} className="btn btn-primacy">INC</button>
+//             <button onClick={rnd} className="btn btn-primacy">RND</button>
+//         </div>
+//     )
+// }
+
+class Counter extends Component {
+    render() {
+        const { counter, inc, dec, rnd } = this.props;
+        return(
+            <div className="jumbotron">
             <h1>{counter}</h1>
             <button onClick={dec} className="btn btn-primacy">DEC</button>
             <button onClick={inc} className="btn btn-primacy">INC</button>
             <button onClick={rnd} className="btn btn-primacy">RND</button>
         </div>
-    )
+        )
+    }
 }
 
-export default Counter;
+const mapStateToProps = (state) => {
+    return {
+        counter: state.value
+    }
+}
+
+export default connect(mapStateToProps, actions)(Counter);
